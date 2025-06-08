b1250ed262646009bf2541c36795c9a0b3fbeb27
DE-MAJESTICConcept <149531935+DE-MAJESTICConcept@users.noreply.github.com>
2025-06-08 13:56:22 -0700

    Update getUserFromStorage.js

--- frontend/src/utils/getUserFromStorage.js
+++ frontend/src/utils/getUserFromStorage.js
@@ -1,4 +1,15 @@
+
 export const getUserFromStorage = () => {
-  const token = JSON.parse(localStorage.getItem("userInfo") || null);
-  return token?.token;
+  try {
+    const userInfoString = localStorage.getItem("userInfo");
+    if (userInfoString) {
+      const userInfo = JSON.parse(userInfoString);
+      return userInfo.token; // Assuming your user info object stores the token directly
+    }
+  } catch (e) {
+    console.error("Error parsing user info from local storage", e);
+    // You might want to clear localStorage here if it's corrupted
+    localStorage.removeItem("userInfo");
+  }
+  return null; // Return null if no user info or token found
 };
