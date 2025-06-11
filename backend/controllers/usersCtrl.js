181ab2d4ac79d5073d9cd3f728396cbb87c6ac98
DE-MAJESTICConcept <149531935+DE-MAJESTICConcept@users.noreply.github.com>
2025-06-11 14:38:46 -0700

    Update usersCtrl.js

--- backend/controllers/usersCtrl.js
+++ backend/controllers/usersCtrl.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../model/User");
-const sendEmail = require("../utils/emailService");
+const sendEmail = require("../utils/emailSender.js");
 const Transaction = require("../model/Transaction");
 // ! REMOVED: const puppeteer = require('puppeteer-core');
 // ! REMOVED: const chrome = require('chrome-aws-lambda');
