7c033105373ad3c7894c19f557ae2ef9f374a09b
DE-MAJESTICConcept <149531935+DE-MAJESTICConcept@users.noreply.github.com>
2025-06-11 12:50:31 -0700

    Update url.js

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-export const BASE_URL = "http://localhost:8000/api/v1"
-=======
+
 export const BASE_URL = "https://mern-expenses-project-backend.onrender.com/api/v1";
->>>>>>> 1301c6bf6b8faf08ed20d7831e71f1bf56e380f1
+
